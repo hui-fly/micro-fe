e312cc87074b57a130143ae0a6708abcee1570c2
zhoudonghui <zhoudonghui@qutoutiao.net>
2020-05-05 21:07:55 +0800

    feat: react模块

--- mainApp/src/assets/js/appList.js
+++ mainApp/src/assets/js/appList.js
@@ -3,22 +3,22 @@ export default {
         id: 1,
         parent: {
             id: 3,
-            name: 'react'
+            name: 'demo管理'
         },
-        name: 'react模块',
-        path: 'https://hui-fly.github.io/micro-react-demo/js/app.js',
+        name: 'vue模块',
+        path: 'https://hui-fly.github.io/micro-module-demo/js/app.js',
         permission: '/testPerm',
-        router: '/react'
+        router: '/vue'
     },
     demo: {
         id: 0,
         parent: {
             id: 3,
-            name: 'vue'
+            name: 'demo管理'
         },
-        name: 'vue模块',
-        path: 'https://hui-fly.github.io/micro-module-demo/js/app.js',
+        name: 'react模块',
+        path: 'https://hui-fly.github.io/micro-react-demo/js/app.js',
         permission: '/demoPerm',
-        router: '/vue'
+        router: '/react'
     }
 };
