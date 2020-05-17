b2e837b6ee97d9573195ecccbc8e318de1c98cc5
zhoudonghui <zhoudonghui@qutoutiao.net>
2020-05-17 16:51:19 +0800

    feat: update

--- apps/react-demo/src/index.js
+++ apps/react-demo/src/index.js
@@ -13,21 +13,10 @@ const reactLifecycles = singleSpaReact({
                 Error
             </div>
         );
-    },
-    domElementGetter
+    }
 });
 
 export const bootstrap = reactLifecycles.bootstrap;
 export const mount = reactLifecycles.mount;
 export const unmount = reactLifecycles.unmount;
 
-function domElementGetter() {
-    let el = document.getElementById("navbar");
-    if (!el) {
-        el = document.createElement("nav");
-        el.id = "navbar";
-        el.className = "topnav";
-        document.body.appendChild(el);
-    }
-    return el;
-}
